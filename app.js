07589a00b507383677aaaa71f22b92de00d05f58
amenibensaada <amenisaada19@gmail.com>
2021-02-19 21:39:59 +0100

    changed to Mongoose

--- app.js
+++ app.js
@@ -1,33 +1,48 @@
 const express = require("express");
-let products = [];
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: String,
+    description: String, 
+    price:Number
+  });
+
+const Product = mongoose.model('Product', productSchema);
+
+
+// let products = [];
 
 const app = express();
 app.use(express.json());
 //app.use("/", (req, res) => 
 //res.send("Hello world!"));
 
-app.get("/products", (req, res) => {
+app.get("/products", async (req, res) => {
+    const products = await Product.find();
     res.json(products);
   });
   
-  app.post("/products", (req, res) => {
+  app.post("/products", async(req, res) => {
     const body = req.body;
-    products.push(body);
+    // products.push(body);
+     await Product.create(body);
     res.json(body);
   });
 
-  app.delete("/products/:id", (req, res) => {
+  app.delete("/products/:id", async (req, res) => {
     const id = req.params.id;
-    const filtredProducts = products.filter((value, index) => index != id);
-    products = filtredProducts;
-    res.json(filtredProducts);
+    // const filtredProducts = products.filter((value, index) => index != id);
+    // products = filtredProducts;
+    const deletedProduct = await Product.findByIdAndDelete(id);
+    res.json(deletedProduct);
   });
 
-  app.patch("/products/:id", (req, res) => {  
+  app.patch("/products/:id", async (req, res) => {  
     const id = req.params.id;
     const body = req.body;
-    products[id] = body;
-    res.json(products);
+    const updatedProduct = await Product.findByIdAndUpdate(id , body); 
+    // products[id] = body;
+    res.json(updatedProduct);
   });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const app = require("./app");
+const mongoose = require('mongoose');
+
 const port = 8000;
+mongoose.connect('mongodb://localhost:27017/bmerce',
+{
+ useNewUrlParser: true,
+ useCreateIndex: true,
+ useFindAndModify: false,
+ useUnifiedTopology: true})
+ .then(()=>console.log('DB connected successfully '))
+ .catch((e)=>console.log(e));
 
 app.listen(port, () => {
 
